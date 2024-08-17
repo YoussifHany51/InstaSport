c12337b355ba308255745843693e7ee7712ae547
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-08-17 17:36:04 +0300

    adding viewController of UpComingEventCell

--- InstaSport/Module/LeagueDetails/View/CellViews/UpComingEventCell/View/UpComingEventCell.swift
+++ InstaSport/Module/LeagueDetails/View/CellViews/UpComingEventCell/View/UpComingEventCell.swift
@@ -16,9 +16,33 @@ class UpComingEventCell: UICollectionViewCell {
     @IBOutlet weak var lblHomeTeam: UILabel!
     @IBOutlet weak var imgViewAwayTeam: UIImageView!
     @IBOutlet weak var imgViewHomeTeam: UIImageView!
+    var viewModel:UpComingEventViewModel?
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    func putData(){
+        if let imgStr = viewModel?.obj.homeTeamLogo{
+            if let url = URL(string:imgStr){
+                imgViewHomeTeam.kf.setImage(with: url)
+            }else{
+                imgViewHomeTeam.image=UIImage(named: "imgFB")
+            }}else{
+                imgViewHomeTeam.image=UIImage(named: "imgFB")
+            }
+        if let imgStr = viewModel?.obj.awayTeamLogo{
+            if let url = URL(string:imgStr){
+                imgViewAwayTeam.kf.setImage(with: url)
+            }else{
+                imgViewAwayTeam.image=UIImage(named: "imgFB")
+            }}else{
+                imgViewAwayTeam.image=UIImage(named: "imgFB")
+            }
+        lblAwayTeam.text=viewModel?.obj.eventAwayTeam
+        lblHomeTeam.text=viewModel?.obj.eventHomeTeam
+        lblStadium.text=viewModel?.obj.eventStadium
+        lblEventDate.text=viewModel?.obj.eventDate
+        lblEventTime.text=viewModel?.obj.eventTime
     }
 
 }
