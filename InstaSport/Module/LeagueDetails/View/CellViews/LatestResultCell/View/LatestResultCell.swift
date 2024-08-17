13fae86915697ca3e5bfeb7ed4679e357d0fbcac
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-08-17 17:42:10 +0300

    adding ViewController of LatestResultCell

--- InstaSport/Module/LeagueDetails/View/CellViews/LatestResultCell/View/LatestResultCell.swift
+++ InstaSport/Module/LeagueDetails/View/CellViews/LatestResultCell/View/LatestResultCell.swift
@@ -6,10 +6,8 @@
 //
 
 import UIKit
-
+import Kingfisher
 class LatestResultCell: UICollectionViewCell {
-
-    
     @IBOutlet weak var lblAwayTeam: UILabel!
     @IBOutlet weak var imgViewAwayTeam: UIImageView!
     @IBOutlet weak var lblStadium: UILabel!
@@ -18,9 +16,37 @@ class LatestResultCell: UICollectionViewCell {
     @IBOutlet weak var lblEventDate: UILabel!
     @IBOutlet weak var lblHomeTeam: UILabel!
     @IBOutlet weak var imgViewHomeTeam: UIImageView!
+    var viewModel:LatestResultViewModel?
     override func awakeFromNib() {
         super.awakeFromNib()
+        
         // Initialization code
+    }
+    func putData(){
+        if let imgStr = viewModel?.obj.homeTeamLogo{
+            if let url = URL(string:imgStr){
+                imgViewHomeTeam.kf.setImage(with: url)
+            }else{
+                imgViewHomeTeam.image=UIImage(named: "imgFB")
+            }}else{
+                imgViewHomeTeam.image=UIImage(named: "imgFB")
+            }
+//        if let imgStr = viewModel?.obj.awayTeamLogo{
+        if let imgStr = viewModel?.obj.awayTeamLogo{
+            if let url = URL(string:imgStr){
+                imgViewAwayTeam.kf.setImage(with: url)
+            }else{
+                imgViewAwayTeam.image=UIImage(named: "imgFB")
+            }}else{
+                imgViewAwayTeam.image=UIImage(named: "imgFB")
+            }
+
+        lblAwayTeam.text=viewModel?.obj.eventAwayTeam
+        lblHomeTeam.text=viewModel?.obj.eventHomeTeam
+        lblStadium.text=viewModel?.obj.eventStadium
+        lblScore.text=viewModel?.obj.eventFinalResult
+        lblEventDate.text=viewModel?.obj.eventDate
+        lblEventTime.text=viewModel?.obj.eventTime
     }
 
 }
