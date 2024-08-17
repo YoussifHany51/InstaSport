c17fd5f58b73ecf50b90fec25a3bbe7e8bd7aff8
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-08-17 17:32:18 +0300

    adding LeaguesDetailsViewModel

--- InstaSport/Module/LeagueDetails/ViewModel/LeagueDetailsViewModel.swift
+++ InstaSport/Module/LeagueDetails/ViewModel/LeagueDetailsViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class LeagueDetailsViewModel{
+    var arrUpComingEvents : [EventModel] = []
+    var arrlastEvent : [EventModel] = []
+    var leagueNum:String
+    var sport:Sports
+    init(sport:Sports,leagueNum:String){
+        self.sport=sport
+        self.leagueNum=leagueNum
+        //getData()
+    }
+    func getData(_ handler:@escaping()->Void){
+        DataParser().parsingFBData(ClassType: EventResult.self, checkSportOrLeague: false, checkUpComingOrLastEvents: true,leagueId: leagueNum, sport: sport) { decodedData in
+            let res = decodedData
+            self.arrUpComingEvents=res.result
+            print("Try to decode Upcoming")
+            handler()
+            
+        }
+        DataParser().parsingFBData(ClassType: EventResult.self, checkSportOrLeague: false, checkUpComingOrLastEvents: false, leagueId: leagueNum,sport: sport) { decodedData in
+            let res = decodedData
+            self.arrlastEvent=res.result
+            print("Try to decode LastEvent")
+            handler()
+        }
+    }
+}
