db80abfde212fd2ebcf918bf940f593268568aa5
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-08-18 03:48:45 +0300

    Finish editing the orthognalView to support the third Section

--- InstaSport/Module/LeagueDetails/Model/TeamsSet.swift
+++ InstaSport/Module/LeagueDetails/Model/TeamsSet.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+struct TeamsSet:Hashable{
+    let imgUrl:String
+    let teamKey:String
+    let teamName:String
+    init(imgUrl: String, teamKey: String,teamName:String) {
+        self.imgUrl = imgUrl
+        self.teamKey = teamKey
+        self.teamName = teamName
+    }
+    static func == (lhs: TeamsSet, rhs: TeamsSet) -> Bool {
+            return lhs.teamKey == rhs.teamKey
+        }
+    func hash(into hasher: inout Hasher) {
+            hasher.combine(teamKey)
+            
+        }
+}
