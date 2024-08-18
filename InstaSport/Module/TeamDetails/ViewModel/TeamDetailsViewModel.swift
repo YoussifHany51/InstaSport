be7d38f31532b57713f00cd23b93c7e9cfe38813
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-08-18 19:40:06 +0300

    adding TeamDetailsViewModel class

--- InstaSport/Module/TeamDetails/ViewModel/TeamDetailsViewModel.swift
+++ InstaSport/Module/TeamDetails/ViewModel/TeamDetailsViewModel.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+class TeamDetailsViewModel{
+    //var teamResult:TeamsResult?
+    var teamDetails:TeamModel?
+    var teamKey:String
+    var sport:Sports
+    init(teamKey:String,sport:Sports){
+        self.teamKey = teamKey
+        self.sport = sport
+    }
+    func getData(_ handler:@escaping()->Void){
+        DataParser().ParsingTeamData( sport: sport, teamKey: teamKey) { decodedData in
+            let res = decodedData
+            self.teamDetails = res.result.first
+            handler()
+            
+        }
+    }
+    
+}
