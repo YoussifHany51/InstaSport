dc9b82b7ed195f20511918be2bbb8df2be8e41b5
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-08-18 03:47:16 +0300

    add array of TeamsSet to LeagueDetails ViewModel and fill it with teams of Leagu

--- InstaSport/Module/LeagueDetails/ViewModel/LeagueDetailsViewModel.swift
+++ InstaSport/Module/LeagueDetails/ViewModel/LeagueDetailsViewModel.swift
@@ -6,10 +6,10 @@
 //
 
 import Foundation
-
 class LeagueDetailsViewModel{
     var arrUpComingEvents : [EventModel] = []
     var arrlastEvent : [EventModel] = []
+    var setTeams :Set<TeamsSet>=[]
     var leagueNum:String
     var sport:Sports
     init(sport:Sports,leagueNum:String){
@@ -22,13 +22,26 @@ class LeagueDetailsViewModel{
             let res = decodedData
             self.arrUpComingEvents=res.result
             print("Try to decode Upcoming")
+            for item in self.arrUpComingEvents{
+                let obj = TeamsSet(imgUrl: item.homeTeamLogo ?? "imgFB", teamKey: "\(item.homeTeamKey)", teamName: item.eventHomeTeam)
+                let obj2 = TeamsSet(imgUrl: item.awayTeamLogo ?? "imgFB", teamKey: "\(item.awayTeamKey)", teamName: item.eventAwayTeam)
+                self.setTeams.insert(obj)
+                self.setTeams.insert(obj2)
+                print(self.setTeams.count)
+            }
             handler()
-            
         }
         DataParser().parsingFBData(ClassType: EventResult.self, checkSportOrLeague: false, checkUpComingOrLastEvents: false, leagueId: leagueNum,sport: sport) { decodedData in
             let res = decodedData
             self.arrlastEvent=res.result
             print("Try to decode LastEvent")
+            for item in self.arrUpComingEvents{
+                let obj = TeamsSet(imgUrl: item.homeTeamLogo ?? "imgFB", teamKey: "\(item.homeTeamKey)", teamName: item.eventHomeTeam)
+                let obj2 = TeamsSet(imgUrl: item.awayTeamLogo ?? "imgFB", teamKey: "\(item.awayTeamKey)", teamName: item.eventAwayTeam)
+                self.setTeams.insert(obj)
+                self.setTeams.insert(obj2)
+                print(self.setTeams.count)
+            }
             handler()
         }
     }
