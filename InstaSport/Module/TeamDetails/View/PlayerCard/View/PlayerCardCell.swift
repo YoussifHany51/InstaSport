416a1a4d26b2a6277a05d3349d4f819fb6ce0c70
Zyad-Abdelbaset <zyad121215@gmail.com>
2024-08-18 19:43:28 +0300

    adding Class of PlayerCell

--- InstaSport/Module/TeamDetails/View/PlayerCard/View/PlayerCardCell.swift
+++ InstaSport/Module/TeamDetails/View/PlayerCard/View/PlayerCardCell.swift
@@ -10,11 +10,35 @@ import UIKit
 class PlayerCardCell: UICollectionViewCell {
 
     @IBOutlet weak var lblPosition: UILabel!
+    
+    @IBOutlet weak var lblRate: UILabel!
     @IBOutlet weak var lblName: UILabel!
+    
     @IBOutlet weak var imgViewPlayer: UIImageView!
+    
+    @IBOutlet weak var lblNum: UILabel!
+    var viewModel : PlayerCardViewModel?
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    func putData(){
+        if let imgStr = viewModel?.player.playerImage{
+            if let url = URL(string:imgStr){
+                imgViewPlayer.kf.setImage(with: url)
+            }else{
+                imgViewPlayer.image=UIImage(named: "ImgPlayer")
+            }}else{
+                imgViewPlayer.image=UIImage(named: "ImgPlayer")
+            }
+//        if let imgStr = viewModel?.obj.awayTeamLogo{
+
+        //print(viewModel!.player.playerName)
+        lblName.text=viewModel?.player.playerName
+        lblPosition.text=viewModel?.player.playerType.rawValue
+        lblNum.text = viewModel?.player.playerNumber
+        lblRate.text = viewModel?.player.playerRating
+        
     }
 
 }
